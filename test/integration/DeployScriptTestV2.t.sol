e41863ae4fc1299ae918b957db93a5210e554c44
hackflu <satyamkumar45381@gmail.com>
2026-02-13 18:58:28 +0000

    add : made change in DeployScriptTestV2

--- test/integration/DeployScriptTestV2.t.sol
+++ test/integration/DeployScriptTestV2.t.sol
@@ -5,8 +5,10 @@ import {UpgradeImplementationV2} from "../../script/DeployImplementationV2.s.sol
 
 contract DeployScriptTestV2 is Test {
     UpgradeImplementationV2 deployScript;
-
+    string ANVIL_URL = "http://127.0.0.1:8545";
     function setUp() public {
+        uint256 fork = vm.createFork(ANVIL_URL);
+        vm.selectFork(fork);
         deployScript = new UpgradeImplementationV2();
     }
 
