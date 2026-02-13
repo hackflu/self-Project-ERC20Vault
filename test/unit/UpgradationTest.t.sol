6a384e7ad140bcf8446ef46573405e66e0f62468
hackflu <satyamkumar45381@gmail.com>
2026-02-13 15:32:46 +0000

    add : _calculateExtraReward test

--- test/unit/UpgradationTest.t.sol
+++ test/unit/UpgradationTest.t.sol
@@ -10,18 +10,18 @@ import {
 } from "lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract ERC20VaultTestV2 is Test {
-    ERC20Vault public vault;
     ERC20VaultV2 public upgradedVault;
     RewardToken public rewardToken;
     DeployImplementationV1 public deployScript;
     UpgradeImplementationV2 public deployScriptForUpgrade;
     ERC1967Proxy public proxy;
 
-    address public owner = makeAddr("owner");
+    address public alice = makeAddr("alice");
+    address public sia = makeAddr("sia");
 
     function setUp() public {
         deployScript = new DeployImplementationV1();
-        (vault, rewardToken, proxy) = deployScript.deployCode();
+        (, rewardToken, proxy) = deployScript.deployCode();
         console.log("address proxy : ", address(proxy));
         console.log("owner of ERC20vault : ", ERC20Vault(address(proxy)).owner());
         deployScriptForUpgrade = new UpgradeImplementationV2();
@@ -29,6 +29,18 @@ contract ERC20VaultTestV2 is Test {
         console.log("owner of deployScript : ", ERC20VaultV2(address(proxy)).owner());
     }
 
+    modifier requiredToDepositAndWithdrawl() {
+        vm.deal(alice, 1 ether);
+        vm.startPrank(alice);
+        ERC20Vault(address(proxy)).deposit{value: 1 ether}();
+        vm.warp(1 days);
+        ERC20Vault(address(proxy)).withdraw(1e18);
+
+        vm.stopPrank();
+        console.log("alice balance : ", alice.balance);
+        console.log("reward token balance : ", rewardToken.balanceOf(alice));
+        _;
+    }
     function testUpgrade() public {
         vm.prank(ERC20Vault(address(proxy)).owner());
         ERC20Vault(address(proxy)).transferOwnership(address(deployScriptForUpgrade));
@@ -37,6 +49,29 @@ contract ERC20VaultTestV2 is Test {
         console.log("owner of deployScript : ", ERC20VaultV2(address(proxy)).owner());
         assertEq(ERC20VaultV2(address(proxy)).owner(), address(deployScriptForUpgrade));
         assertEq(proxies, address(proxy));
+    }
+
+    /**
+     * @notice to check the upgraded _calculateReward function
+     */
+    function testCalculateReward() public requiredToDepositAndWithdrawl{
+
+        // 1. upgrade the code
+        vm.prank(ERC20Vault(address(proxy)).owner());
+        ERC20Vault(address(proxy)).transferOwnership(address(deployScriptForUpgrade));
+        vm.prank(address(deployScriptForUpgrade));
+        address proxies = deployScriptForUpgrade.UpgradeImplementationV1(address(proxy), address(upgradedVault));
+
+        //3. another deposit (SIA)
+        vm.deal(sia , 1 ether);
+        vm.startPrank(sia);
+        ERC20VaultV2(address(proxies)).deposit{value: 1 ether}();
+        vm.warp(1 days);
+        ERC20VaultV2(address(proxies)).withdraw(1e18);
+        vm.stopPrank();
+        console.log("sia balance : ", sia.balance);
+        console.log("reward token of sia balance : ", rewardToken.balanceOf(sia));
+        assertGe(rewardToken.balanceOf(sia),rewardToken.balanceOf(alice));
     }
 }
 
